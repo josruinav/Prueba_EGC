ef987dec526746df8427e47d47272f38d159194f
josruinav <jose.9.viso@gmail.com>
2017-01-13 02:02:14 +0100

    Proyecto subido con test

---
+++ Prueba_EGC/src/main/java/EGC/Prueba_EGC/Prueba.java
@@ -0,0 +1,22 @@
+package EGC.Prueba_EGC;
+
+
+public  class Prueba {
+
+
+	public static double factorial(double n){
+		
+		double factorial = 1.0;
+		
+		while ( n!=0) {
+			  factorial=factorial*n;
+			  n--;
+			}
+		
+		return factorial;
+		
+		
+	}
+	
+	
+}
---
+++ Prueba_EGC/src/test/java/EGC/Prueba_EGC/PruebaTest.java
@@ -0,0 +1,28 @@
+package EGC.Prueba_EGC;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import junit.framework.Assert;
+
+
+
+public class PruebaTest{
+
+
+	
+	@Test
+	public void testPruebaEGC(){
+		
+		Prueba prueba = new Prueba();
+		
+		double result = Prueba.factorial(8);
+		
+		System.out.println(result);
+		
+		Assert.assertTrue(result == 40320.0);
+		
+		
+	
+	}
+}
